a7b9df96657a697b4c18ed75a063bf865215e932
Hul Makara <noah22012004@gmail.com>
2025-06-10 23:18:43 +0700

    Invoice

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Invoice } from './invoice';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Invoice],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'Invoice_Project';
---
+++ src/app/invoice.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './invoice.html',
+  styleUrls: ['./invoice.css'],
+})
+export class Invoice {
+  company = {
+    name: 'Lucky DFI',
+    address: '2004 St, SenSok, Cambodia',
+    message: 'Open 7/24\nThank you for your business!',
+  };
+
+  receipt = {
+    date: new Date().toLocaleDateString('en-US', {
+      month: '2-digit',
+      day: '2-digit',
+      year: 'numeric',
+    }),
+    items: [
+      { name: 'Orange Juice', price: 32.15 },
+      { name: 'Apples', price: 3.5 },
+      { name: 'Tomato', price: 2.4 },
+      { name: 'Fish', price: 0.9 },
+      { name: 'Beef', price: 10.0 },
+      { name: 'Onion', price: 1.25 },
+      { name: 'Cheese', price: 3.4 },
+    ],
+    tax: 2.89,
+    total: 29.69,
+    barcode: '123456778963578021',
+  };
+
+  get subtotal() {
+    return this.receipt.items.reduce((sum, item) => sum + item.price, 0);
+  }
+}
